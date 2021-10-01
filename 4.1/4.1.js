ed60fc6ce982a55ced58d9dbe8927c931eab93ec
Chris Beck <89880549+Craspar@users.noreply.github.com>
2021-10-01 16:57:15 -0500

    Add files via upload

---
+++ 4.1/4.1.js
@@ -0,0 +1,7 @@
+var x;
+
+for (x = 5; x<16; x++){
+
+    alert(x + " squared is " + Math.pow(x, 2));
+
+}
---
+++ 4.3/4.3.js
@@ -0,0 +1,7 @@
+var x = prompt("please enter a number:");
+var y = prompt("please enter another number:");
+var z = prompt("please enter a final number:");
+var i = Math.max(x,y);
+
+alert("You entered " + x + ", " + y + ", and " + z + ". The largest number is: " + Math.max(i,z));
+
